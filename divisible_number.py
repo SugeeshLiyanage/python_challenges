6bd09d040d347e496acb0dff23b7fea8a29e8bc3
Sugeesh Liyanage <sugeeshdeveloper@gmail.com>
2025-09-01 15:23:07 -0500

    Add files via upload

---
+++ divisible_number.py
@@ -0,0 +1,6 @@
+non = int(input("enter a numerator"))
+dod = int(input("enter a denominator"))
+if non%dod==0:
+ print(str(non)+"is divisible by"+ str(dod))
+else:
+ print(str(non)+"is not divisible by"+ str(dod))
