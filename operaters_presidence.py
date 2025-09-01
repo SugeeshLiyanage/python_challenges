b8fe6fe95697450b7d46b59215e2934dcfaa64e4
Sugeesh Liyanage <sugeeshdeveloper@gmail.com>
2025-09-01 14:59:52 -0500

    Add files via upload

---
+++ operaters_presidence.py
@@ -0,0 +1,14 @@
+w = 4
+e = 9 
+r = 2
+h = 7
+o = 11
+j = 7
+name = (j+o * w / r -j) + e 
+print (name)
+bob = "j"
+thing = 5
+if bob == "j" or bob == "blob" and thing >= 2:
+  print ("yeah")
+else:
+  print("neah")
